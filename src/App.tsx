e5323b15861acc5103f3f4bcae6d24f33858f867
nikolina <nikolina.cesic@nobula.eu>
2023-10-25 19:26:53 +0200

    Fetching test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import styled from "styled-components";
+
+const CalendarContainer = styled.div`
+  width: 50rem;
+  height: 20rem;
+  padding: 1rem;
+`
+
+const Calendar = styled(FullCalendar)`
+  .fc-day fc-day-sat fc-day-disabled fc-daygrid-day {
+    visibility: hidden;
+  }
+`
+const graphqlQuery = `
+  query {
+    repository(owner: "Nikolina2106", name: "calendar-app") {
+      ref(qualifiedName: "refs/heads/main") {
+        target {
+          ... on Commit {
+            history(first: 100) {
+              nodes {
+                committedDate
+                message
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get(`https://api.github.com/repos/Nikolina2106/calendar-app/commits`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                // Handle any errors
+                console.error(error);
+            });
+    }, []);
+    console.log(data);
+
+    return (
+        <CalendarContainer>
+            <Calendar
+                displayEventTime={true}
+                schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+                selectable={true}
+                initialView="dayGridMonth"
+                plugins={[
+                    dayGridPlugin,
+                    timeGridPlugin,
+                    interactionPlugin,
+                    resourceTimeGridPlugin
+                ]}
+                eventClick={event => {
+                    console.log('aaa');
+                }}
+                headerToolbar={{
+                    left: "prev,next today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+                }}
+                showNonCurrentDates={false}
+            />
+        </CalendarContainer>
+    );
 }
 
 export default App;
